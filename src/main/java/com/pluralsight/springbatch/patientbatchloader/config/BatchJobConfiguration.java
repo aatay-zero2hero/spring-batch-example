a1af55532689712f900d3967793b79b6b0d6b26b
mahadyhasan <mahady.hasan@hotmail.com>
2018-07-13 18:35:46 +0100

    first commit

---
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
@@ -0,0 +1,41 @@
+package com.pluralsight.springbatch.patientbatchloader.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchJobConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry);
+        return postProcessor;
+    }
+
+    @Bean
+    public Job job(Step step) throws Exception {
+        return this.jobBuilderFactory
+            .get(Constants.JOB_NAME)
+            .validator(validator())
+            .start(step)
+            .build();
+    }
+
+    @Bean
+    private JobParametersValidator validator() {
+        return null;
+    }
+
+
+}
